61ad83f9e5952132bd7f93aa407143bceb74b7cb
Manuel Angel Rodriguez <115673092+manuelangelrodr@users.noreply.github.com>
2024-02-20 12:49:31 +0100

    actualizacion

--- src/train.py
+++ src/train.py
@@ -1,1 +1,9 @@
 # SCRIPT DE ENTRENAMIENTO DE MODELOS DE MACHINE LEARNING
+
+# IMPORTAMOS LIBRERIAS
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
