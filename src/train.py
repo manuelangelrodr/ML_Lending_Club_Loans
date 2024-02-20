da93a73b5e4287f19fd5d2eb61272d34291785d9
Manuel Angel Rodriguez <115673092+manuelangelrodr@users.noreply.github.com>
2024-02-20 13:25:29 +0100

    prueba

--- src/train.py
+++ src/train.py
@@ -7,3 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+# df = pd.read_csv('../src/data/processed/df_original.csv')
+df2 = pd.read_csv('ML_Lending_Club_Loans\src\data\processed\df_original.csv')
+
+df2.head(10)
