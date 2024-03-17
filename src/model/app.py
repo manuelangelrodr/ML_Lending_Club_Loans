d327713500ebd1ce71cab4ee1052b27c33e24c4e
Manuel Angel Rodriguez <115673092+manuelangelrodr@users.noreply.github.com>
2024-03-17 23:01:33 +0100

    actual

--- src/model/app.py
+++ src/model/app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return "Hola Mundo"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, port=5000)
