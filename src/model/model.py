73bdeb98e4524baf13608cab1fa7c1874bc2a1e2
Manuel Angel Rodriguez <115673092+manuelangelrodr@users.noreply.github.com>
2024-03-19 11:27:25 +0100

    actual

--- src/model/model.py
+++ src/model/model.py
@@ -1,5 +1,5 @@
 # IMPORTAR LIBRERIAS
-
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
@@ -7,12 +7,12 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, recall_score, f1_score, roc_auc_score, confusion_matrix, precision_score
 import pickle
 
-import sys
-sys.path.append('../')
-from src.utils.eda_functions import *
+# import sys
+# sys.path.append('../')
+# from src.utils.eda_functions import *
 
 # CARGAMOS EL DATASET
 
-df = pd.read_csv('../src/data/processed/df_original.csv')
+# df = pd.read_csv('../src/data/processed/df_original.csv')
 
-print(df.head(10))
+# print(df.head(10))
