c2fa569f606a6f85b663178be795ddb808d1f904
Manuel Angel Rodriguez <115673092+manuelangelrodr@users.noreply.github.com>
2024-03-19 14:00:44 +0100

    actual

---
+++ src/model/model.py
@@ -0,0 +1,11 @@
+# IMPORTAR LIBRERIAS
+import numpy as np
+import pandas as pd
+
+# import sys
+# sys.path.append('../')
+# from utils.eda_functions import *
+
+df = pd.read_csv("../processed/df_original.csv")
+
+print(df.head())
--- src/model_changes.py
+++
@@ -1,7 +0,0 @@
-# IMPORTAR LIBRERIAS
-import numpy as np
-import pandas as pd
-
-df = pd.read_csv("src/data/processed/df_original.csv")
-
-print(df.head())
