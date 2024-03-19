a5e0e9c63642086f17b3fd6cc34a721cd23c0a57
Manuel Angel Rodriguez <115673092+manuelangelrodr@users.noreply.github.com>
2024-03-19 14:57:41 +0100

    actualizacion

--- src/model/model.py
+++ src/model/model.py
@@ -1,11 +1,17 @@
 # IMPORTAR LIBRERIAS
 import numpy as np
 import pandas as pd
+import os
+
+file_path = r'C:\Users\manue\OneDrive\Documentos\GitHub\ML_Lending_Club_Loans\src\data\processed\df_original.csv'
 
 # import sys
 # sys.path.append('../')
 # from utils.eda_functions import *
 
-df = pd.read_csv("../processed/df_original.csv")
+
+# Cargar el archivo CSV en un DataFrame de pandas
+df = pd.read_csv(file_path)
+
 
 print(df.head())
